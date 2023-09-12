e228a355516d77c1809f35e506b39a6cb7951974
Fewdys <dipzei.vibes@gmail.com>
2023-09-12 11:29:26 -0400

    Add project files.

---
+++ GetInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using MelonLoader;
+using UnityEngine;
+using ABI_RC.Core.InteractionSystem;
+using ABI_RC.Core.Networking.IO.Social;
+
+namespace GetInfo
+{
+    public class GetInfo : MelonMod
+    {
+        private ViewManager _viewManager { get; set; }
+
+        private UserProfile_t _userProfile { get; set; }
+        public override void OnApplicationStart() => MelonCoroutines.Start(WaitForUi());
+
+        private IEnumerator WaitForUi()
+        {
+            while (GameObject.Find("/Cohtml") == null)
+            {
+                yield return null;
+            }
+            _viewManager = GameObject.Find("/Cohtml/CohtmlWorldView").GetComponent<ViewManager>();
+        }
+        public override void OnUpdate()
+        {
+            if (Input.GetKey(KeyCode.G) && Input.GetKeyDown(KeyCode.I))
+            {
+                _userProfile = (UserProfile_t)typeof(ViewManager).GetField("_userProfile", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(_viewManager);
+                MelonLogger.Msg(System.ConsoleColor.DarkGray, $"{_userProfile.UserName}\nUserID: {_userProfile.UserId}\nRank: {_userProfile.UserRank}" + $"\nAvatarID: {_userProfile.CurrentAvatarId}\nAvatarName: {_userProfile.CurrentAvatarName}");
+                //System.Windows.Forms.Clipboard.SetText(_userProfile.UserId);
+            }
+        }
+
+    }
+}
